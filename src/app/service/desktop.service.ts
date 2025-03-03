6480c4b67092da0113d815b83fbfa23a17fbd9b1
Romain Boulland <romain.boulland@capgemini.com>
2025-03-03 14:45:58 +0100

    Envoyer la date format ISO

--- src/app/service/desktop.service.ts
+++ src/app/service/desktop.service.ts
@@ -100,14 +100,8 @@ export class DesktopService extends ApiService {
   }
 
   // i hate date handling in js
-  getDatePart(date: DateTime): string {
-    /*
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Les mois sont indexés à partir de 0
-    const day = String(date.getDate()).padStart(2, '0');
-    
-    return `${year}-${month}-${day}`; */
-    return date.toLocaleString(DateTime.DATE_SHORT);
+  getDatePart(date: DateTime): string | null {
+    return date.toISODate();
   }
 
   setSelectedDesktop(desktop: Desktop) {
